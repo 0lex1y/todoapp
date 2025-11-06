1b5ac1c4f32051db8db919c4ec7ea0d124d32fa6
Kotov Oleksiy <kotov.oleksiy@icloud.com>
2025-11-06 22:18:53 +0200

    Created admin django and make migrations

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from task.models import Task, Tag
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "created_at", "deadline", "complete")
+    search_fields = ("title",)
+    list_filter = ("deadline", "complete")
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
